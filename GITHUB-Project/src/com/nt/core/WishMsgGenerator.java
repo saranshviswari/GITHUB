5ce02e6445aa68868d3af80d08d8a93a5658b4db
SaranshDev <dev@gmail.com>
2018-06-14 02:02:41 +0530

    tl- stashing commit

--- GITHUB-Project/src/com/nt/core/WishMsgGenerator.java
+++ GITHUB-Project/src/com/nt/core/WishMsgGenerator.java
@@ -2,6 +2,10 @@ package com.nt.core;
 
 public class WishMsgGenerator {
 
+	/*public int get(int){
+		return ;
+	}*/
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Good Morning Saransh!!");
